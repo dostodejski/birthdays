981bfe6d6b55f6bd82348fcb82dbd0ad0312a138
GianlucaBasso <gb.basso14@gmail.com>
2019-12-02 09:54:28 -0500

    Code documentation solved

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,9 @@
+'''
+The goal is to create a dictionary that associates birthdays with the respective famous people, it will be the list used in main.py function
+'''
+
+
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +12,14 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+#This function print the names of people in the dictionary birthdays
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+#This function returns the birthday of the desired person 
+#if the person is not in the list it will give notice to the user with a message
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
