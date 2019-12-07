312fe6285f83a56b1fdefd511ddeaf2fcf8d18d0
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-07 18:18:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3
 import argparse
-parser = argparse.ArgumentParser(description='Write text into the file')
+parser = argparse.ArgumentParser(description='These are the birthdays of Albert Einstein and Alan Turing')
 #define arguments
 parser.add_argument('Albert Einstein', help='Birthday of Albert Einstein')
-#parser.add_argument('Alan Turing', action='store_true', help= 'Birthday of Alan Turing')
+parser.add_argument('Alan Turing', help= 'Birthday of Alan Turing')
 args = parser.parse_args()
 
 from mypackage.birthdays import return_birthday
