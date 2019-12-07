6a6474db40787d1d91871b5d5bb14451ef0f60a3
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-07 18:05:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Write text into the file')
 #define arguments
 parser.add_argument(action='store_true', help='Test help')
+parser.add_argument(choices=[])
 args = parser.parse_args()
 
 from mypackage.birthdays import return_birthday
