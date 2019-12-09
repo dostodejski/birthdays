6461353f49e8b0e6a392d568781b86628b4a4170
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-09 17:08:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python3
 import argparse
+from mypackage.birthdays import return_birthday
+
+return_birthday('Albert Einstein')
+return_birthday('Alan Turing')
+
 parser = argparse.ArgumentParser(description='These are the birthdays of Albert Einstein and Alan Turing')
 #define arguments
 parser.add_argument('birthdays', help='it is his birthday', type= str)
 parser.add_argument('--Alan Turing', help= 'Alan Turing is not in this list')
 args = parser.parse_args()
 
-from mypackage.birthdays import return_birthday
-
-return_birthday('Albert Einstein')
-return_birthday('Alan Turing')
