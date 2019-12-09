d96f61e9255f0031422e3c05a8c25b10575df6af
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-09 17:48:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ parser.add_argument('Albert Einstein', help='it is his birthday', type= str)
 parser.add_argument('--Alan Turing', help= 'Alan Turing is not in this list')
 args = parser.parse_args
 
-from mypackage.birthdays import return_birthday
+#from mypackage.birthdays import return_birthday
 
-return_birthday('Albert Einstein')
-return_birthday('Ada LoveLace')
+#return_birthday('Albert Einstein')
+#return_birthday('Ada LoveLace')
 
 
