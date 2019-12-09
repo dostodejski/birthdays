cac79fcd387ee924a96129d16bbde817d908833a
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-09 23:41:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ birthdays = {
     'Donald Trump': '06/14/1946',
     'Rowan Atkinson': '01/6/1955'}
 
+def return_birthday(name):
+    if name in birthdays:
+        print('{}\'s birthday is {}.'.format(name, birthdays[name]))
+    else:
+        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
+
 import argparse
 #from mypackage.birthdays import return_birthday
 parser = argparse.ArgumentParser(description='program that gives back birthdays of scientists')
