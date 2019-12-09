f07dea0e22738dcc21fee0b75371a422ddbed818
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-09 17:33:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='These are the birthdays of famous people')
 #define arguments
 parser.add_argument('Albert Einstein', help='it is his birthday', type= str)
-#parser.add_argument('--Alan Turing', help= 'Alan Turing is not in this list')
+parser.add_argument('--Alan Turing', help= 'Alan Turing is not in this list')
 args = parser.parse_args
 
 from mypackage.birthdays import return_birthday
