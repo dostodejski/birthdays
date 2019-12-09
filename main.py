396e9d9f59d9bc37d22e1f0e8ec942d1c23892f5
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-09 18:20:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity")
 #parser.add_argument('--Alan Turing', help= 'Alan Turing is not in this list')
 ciao=return_birthday('Albert Einstein')
-args = parser.parse_args('Albert Einstein')
+args = parser.parse_args()
 print (args, ciao)
 #return_birthday('Ada Lovelace')
 
