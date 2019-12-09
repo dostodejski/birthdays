25883396d02b424e7e37f4e6d500b57898ad14a5
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-09 22:47:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import argparse
 from mypackage.birthdays import return_birthday
 parser = argparse.ArgumentParser(description='These are the birthdays of famous people')
 #define arguments
-parser.add_argument('Albert Einstein', help='it is his birthday', type=str)
-parser.add_argument("-v", "--verbose", action="store_true",
-                    help="increase output verbosity")
+parser.add_argument('Albert Einstein', action= "store_true", default=None,help='was famous')
+#parser.add_argument("-v", "--verbose", action="store_true",
+                   # help="increase output verbosity")
 #parser.add_argument('--Alan Turing', help= 'Alan Turing is not in this list')
-ciao=return_birthday('Albert Einstein')
+#ciao=return_birthday('Albert Einstein')
 args = parser.parse_args()
-print (args, ciao)
+#print (args, ciao)
 #return_birthday('Ada Lovelace')
 
 
