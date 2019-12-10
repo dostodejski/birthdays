b950932c1f5be427ec29b1ab8145b389437791c7
GianlucaBasso <57446011+GianlucaBasso@users.noreply.github.com>
2019-12-10 11:38:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ parser = argparse.ArgumentParser(description='program that gives back birthdays 
 parser.add_argument("name", help= "insert one of the scientist name of the list", type=str, default=None)
 parser.add_argument("--birthdays", type=str, help="Names inside the birthday dictionary", choices = ["Albert Einstein", "Benjamin Franklin", "Ada Lovelace", "Donald Trump", "Rowan Atkinson"])
 args = parser.parse_args()
-risultato = return_birthday(args.name)
+result = return_birthday(args.name)
 
 
 
