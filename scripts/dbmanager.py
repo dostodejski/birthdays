257f3e4b7b966f797be9d77922e6a41f3d665fb7
garbina98 <57446165+garbina98@users.noreply.github.com>
2019-12-12 19:02:21 +0100

    Update dbmanager.py

--- scripts/dbmanager.py
+++ scripts/dbmanager.py
@@ -1,11 +1,5 @@
 import hashlib
 import random
-CREATE TABLE user (username CHAR(256), password CHAR(256));
-CREATE TABLE profile (username CHAR(256), role CHAR(256));
-INSERT INTO user VALUES("Alan", "123");
-INSERT INTO profile VALUES("Alan", "admin");
-INSERT INTO user VALUES("Albert", "1234");
-INSERT INTO profile VALUES("Albert", "user");
 password = "the password typed by the user"
 salt=str(random.random())
 digest = salt + password
